f7b2781bb01a5857bb9c4f55d6f4b5397cc3e091
jetrosa <jetrosa@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-04 12:03:19 +0000

    koodin testausta (väri ei toimi)
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@13 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht2/src/lejosSensoring/ColorCalibration.java
+++ EV3teht2/src/lejosSensoring/ColorCalibration.java
@@ -3,6 +3,10 @@ package lejosSensoring;
 public class ColorCalibration {
 	private float[][] colors = new float[3][3];
 	
+	public void calibrateFloor(float[] floor) {
+		colors[0] = floor;
+	}
+	
 	public void calibrateLine1(float[] line1) {
 		colors[1] = line1;
 	}
@@ -11,19 +15,37 @@ public class ColorCalibration {
 		colors[2] = line2;
 	}
 	
-	public void calibrateFloor(float[] floor) {
-		colors[0] = floor;
-	}
-	
 	public int testColor(float[] color) {
+		
+		System.out.println();
+		System.out.println(colors[0][0]);
+		System.out.print(colors[0][1]);
+		System.out.print(colors[0][2]);
+		System.out.println(colors[1][0]);
+		System.out.print(colors[1][1]);
+		System.out.print(colors[1][2]);
+		System.out.println(colors[2][0]);
+		System.out.print(colors[2][1]);
+		System.out.print(colors[2][2]);
+		System.out.println();
 		
 		float[] results = new float[3];
 		double[] endResults = new double[3];
 		
 		for (int i = 0; i < colors.length; i++) {
 			for (int n = 0; n < colors[i].length; n++) {
+				System.out.println(colors[i][n]);
+				colors[i][n]*=765;
+				color[n]*=765;
 				
-				results[n] = Math.round(color[n]) - colors[i][n];
+				results[n] = color[n] - colors[i][n];
+				
+				//System.out.println(color[n]);
+				System.out.println(colors[i][n]);
+				//System.out.println();	
+				//System.out.println(results[n]);
+				//System.out.println();	
+				
 				results[n] *= results[n];
 				
 			}
@@ -39,6 +61,13 @@ public class ColorCalibration {
 			}
 			
 		}
-		return closest;
+		/*
+		System.out.println(endResults[0]);
+		System.out.println(endResults[1]);
+		System.out.println(endResults[2]);
+		System.out.println();
+		*/
+		
+		return closest;		
 	}
 }
--- EV3teht2/src/lejosSensoring/teht2main.java
+++ EV3teht2/src/lejosSensoring/teht2main.java
@@ -1,16 +1,24 @@
 package lejosSensoring;
 
 import lejos.hardware.Button;
+import lejos.hardware.Device;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorModes;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class teht2main {
 	public static void main(String[] args) {
+		
+		RegulatedMotor motor1 = new EV3LargeRegulatedMotor(MotorPort.B);
+		RegulatedMotor motor2 = new EV3LargeRegulatedMotor(MotorPort.C);
+		motor1.synchronizeWith(new RegulatedMotor[] { motor2 });
 		
 		Port wallPort = LocalEV3.get().getPort("S4");
 		SensorModes wallSensor = new EV3IRSensor(wallPort);
@@ -21,11 +29,10 @@ public class teht2main {
 		SensorModes colorSensor = new EV3ColorSensor(colorPort);
 		SampleProvider colorProvider = ((EV3ColorSensor)colorSensor).getRGBMode();
 		float[] colorSample = new float[colorProvider.sampleSize()];
-		System.out.println("Waiting for a button press");
-		Button.ENTER.waitForPressAndRelease();
 		
 		int colorMultiplier = 765;
 		boolean isWallSeen = false;
+		boolean isLooping = true;
 		ColorCalibration colorTester = new ColorCalibration();
 		
 		System.out.println("Show the color of the floor, and then press the button");
@@ -43,18 +50,23 @@ public class teht2main {
 		colorProvider.fetchSample(colorSample, 0);
 		colorTester.calibrateLine2(colorSample);
 		
-		while(true) {
+		System.out.println("Press the button to start");
+		Button.ENTER.waitForPressAndRelease();
+		move(motor1, motor2, 400, 400, 0);
+		
+		while(isLooping) {
 			
+			Delay.msDelay(1000);
 			colorProvider.fetchSample(colorSample, 0);
 			distance.fetchSample(wallSample, 0);
 			
 			int testResult = colorTester.testColor(colorSample);
 			
 			switch(testResult) {
-			
 			case 1: 
 				if (isWallSeen) {
-					//stop
+					stopMoving(motor1, motor2);
+					isLooping = false;
 				}
 				System.out.println("First line seen");
 				break;
@@ -62,12 +74,38 @@ public class teht2main {
 				System.out.println("Second line seen");
 			}
 			
-			if(wallSample[0] <= 10) {
+			if(!isWallSeen && wallSample[0] <= 10) {
 				isWallSeen = true;
+				move(motor1, motor2, 400, 400, 1);
 			}
 			
 			Delay.msDelay(100);
 		}
+		((Device) wallSensor).close();
+		((Device) colorSensor).close();	
+		motor1.close();
+		motor2.close();
+	}
+	
+	private static void move(RegulatedMotor m1, RegulatedMotor m2, int leftSpeed, int rightSpeed, int changeDirection) {
+		m1.setSpeed(leftSpeed);
+		m2.setSpeed(rightSpeed);
+		m1.startSynchronization();
+		if(changeDirection == 0) {
+			m1.forward();
+			m2.forward();
+		}else {
+			m1.backward();
+			m2.backward();
+		}
 		
+		m1.endSynchronization();
+	}
+	
+	private static void stopMoving(RegulatedMotor m1, RegulatedMotor m2) {
+		m1.startSynchronization();
+		m1.stop();
+		m2.stop();
+		m1.endSynchronization();
 	}
 }
