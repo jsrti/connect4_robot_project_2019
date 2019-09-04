c496727d8ce66570f7f2012f2171101621fbfca0
kimwi <kimwi@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-04 09:50:01 +0000

    Added main loop and calibration
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@11 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht2/src/lejosSensoring/teht2main.java
+++ EV3teht2/src/lejosSensoring/teht2main.java
@@ -11,49 +11,63 @@ import lejos.utility.Delay;
 
 public class teht2main {
 	public static void main(String[] args) {
-		/*
-		Port port = LocalEV3.get().getPort("S4");
-		SensorModes sensor = new EV3IRSensor(port);
-		SampleProvider distance = ((EV3IRSensor)sensor).getDistanceMode();
-		float[] sample = new float[distance.sampleSize()];
-		while(true) {
-			distance.fetchSample(sample, 0);
-			System.out.println(sample[0]);
-		}
-		*/
-		int colorMultiplier = 765;
-		int max = 255;
+		
+		Port wallPort = LocalEV3.get().getPort("S4");
+		SensorModes wallSensor = new EV3IRSensor(wallPort);
+		SampleProvider distance = ((EV3IRSensor)wallSensor).getDistanceMode();
+		float[] wallSample = new float[distance.sampleSize()];
 		
 		Port colorPort = LocalEV3.get().getPort("S1");
 		SensorModes colorSensor = new EV3ColorSensor(colorPort);
 		SampleProvider colorProvider = ((EV3ColorSensor)colorSensor).getRGBMode();
 		float[] colorSample = new float[colorProvider.sampleSize()];
+		System.out.println("Waiting for a button press");
+		Button.ENTER.waitForPressAndRelease();
+		
+		int colorMultiplier = 765;
+		boolean isWallSeen = false;
+		ColorCalibration colorTester = new ColorCalibration();
+		
+		System.out.println("Show the color of the floor, and then press the button");
+		Button.ENTER.waitForPressAndRelease();
+		colorProvider.fetchSample(colorSample, 0);
+		colorTester.calibrateFloor(colorSample);
+		
+		System.out.println("Show the color of the first line, and then press the button");
+		Button.ENTER.waitForPressAndRelease();
+		colorProvider.fetchSample(colorSample, 0);
+		colorTester.calibrateLine1(colorSample);
+		
+		System.out.println("Show the color of the second line, and then press the button");
+		Button.ENTER.waitForPressAndRelease();
+		colorProvider.fetchSample(colorSample, 0);
+		colorTester.calibrateLine2(colorSample);
+		
 		while(true) {
-			System.out.println("Waiting for a button press");
-			Button.ENTER.waitForPressAndRelease();
+			
 			colorProvider.fetchSample(colorSample, 0);
-
-			int r = Math.round(colorSample[0]*colorMultiplier);
-			int g = Math.round(colorSample[1]*colorMultiplier);
-			int b = Math.round(colorSample[2]*colorMultiplier);
-
-			int rv = max - r;
-			int gv = max - g;
-			int bv = max - b;
+			distance.fetchSample(wallSample, 0);
 			
-			System.out.println(r + " " + g + " " + b);
-			if (rv < gv && rv < bv) {
-				System.out.println("red");
-			}
-			else if (gv < rv && gv < bv) {
-				System.out.println("green");
-			}
-			else if (bv < rv && bv < gv) {
-				System.out.println("blue");
+			int testResult = colorTester.testColor(colorSample);
+			
+			switch(testResult) {
+			
+			case 1: 
+				if (isWallSeen) {
+					//stop
+				}
+				System.out.println("First line seen");
+				break;
+			case 2:
+				System.out.println("Second line seen");
 			}
 			
+			if(wallSample[0] <= 10) {
+				isWallSeen = true;
+			}
 			
-			Delay.msDelay(1000);
+			Delay.msDelay(100);
 		}
+		
 	}
 }
