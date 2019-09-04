ee052b38174cad46e87e91b023cff7b3a05b77b2
kimwi <kimwi@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-04 09:50:18 +0000

    Changed variable types
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@12 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht2/src/lejosSensoring/ColorCalibration.java
+++ EV3teht2/src/lejosSensoring/ColorCalibration.java
@@ -1,29 +1,29 @@
 package lejosSensoring;
 
 public class ColorCalibration {
-	private int[][] colors = new int[3][3];
+	private float[][] colors = new float[3][3];
 	
-	public void calibrateLine1(int[] line1) {
+	public void calibrateLine1(float[] line1) {
 		colors[1] = line1;
 	}
 	
-	public void calibrateLine2(int[] line2) {
+	public void calibrateLine2(float[] line2) {
 		colors[2] = line2;
 	}
 	
-	public void calibrateFloor(int[] floor) {
+	public void calibrateFloor(float[] floor) {
 		colors[0] = floor;
 	}
 	
-	public int testColor(int[] color) {
+	public int testColor(float[] color) {
 		
-		int[] results = new int[3];
+		float[] results = new float[3];
 		double[] endResults = new double[3];
 		
 		for (int i = 0; i < colors.length; i++) {
 			for (int n = 0; n < colors[i].length; n++) {
 				
-				results[n] = color[n] - colors[i][n];
+				results[n] = Math.round(color[n]) - colors[i][n];
 				results[n] *= results[n];
 				
 			}
