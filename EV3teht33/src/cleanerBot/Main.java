7672434765955be53619bdf58a7a37063f35d099
kimwi <kimwi@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-06 11:14:22 +0000

    main commented
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@53 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht33/src/cleanerBot/Main.java
+++ EV3teht33/src/cleanerBot/Main.java
@@ -16,12 +16,14 @@ public class Main {
 	public static void main(String[] args) {
 		Arbitrator arbitrator;
 		ArrayList<Behavior> behaviors = new ArrayList<>();
-
+		
+		// Gets the sensors and motors from the ports
 		Port colorPort = LocalEV3.get().getPort("S1");
 		Port IRPort = LocalEV3.get().getPort("S4");
 		RegulatedMotor motor1 = new EV3LargeRegulatedMotor(MotorPort.B);
 		RegulatedMotor motor2 = new EV3LargeRegulatedMotor(MotorPort.C);
 
+		// Initializes the needed classes
 		ColorTester colorCalibrator = new ColorTester(colorPort);
 		DistanceSensor distanceSensor = new DistanceSensor(IRPort);
 		Movement movement = new Movement(motor1, motor2);
@@ -31,6 +33,7 @@ public class Main {
 		ObstacleCheck obstacleCheck = new ObstacleCheck(distanceSensor, movement);
 		EmergencyStop emergencyStop = new EmergencyStop();
 
+		// Adds the behaviors in the order of importance from least to most
 		behaviors.add(driveForward);
 		behaviors.add(lineCheck);
 		behaviors.add(obstacleCheck);
@@ -40,12 +43,14 @@ public class Main {
 
 		arbitrator = new Arbitrator(behaviorArray);
 
+		// Starting sequence
 		colorCalibrator.startCalibration();
 		System.out.println("Please test the emergency stop button. (Down)");
 		Button.DOWN.waitForPressAndRelease();
 		System.out.println("yep, it works");
 		System.out.println("Press the button to start");
 		Button.ENTER.waitForPressAndRelease();
+		// Starts the bot's default cycle
 		arbitrator.go();
 
 		((Device) colorPort).close();
