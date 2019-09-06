b992adf4889d69629e0542a3af812cde22346ae2
jetrosa <jetrosa@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-06 06:38:25 +0000

    suunnanvaihto liikkumisessa mahdollinen (Move-metodissa boolean-parametri)
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@34 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht33/src/cleanerBot/Movement.java
+++ EV3teht33/src/cleanerBot/Movement.java
@@ -1,12 +1,15 @@
 package cleanerBot;
 
 import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 
 public class Movement {
 	private RegulatedMotor motor1;
 	private RegulatedMotor motor2;
+	boolean movingForward = true;
+	long startTime;
 
 	public Movement(RegulatedMotor motor1, RegulatedMotor motor2) {
 		this.motor1 = motor1;
@@ -14,20 +17,25 @@ public class Movement {
 		motor1.synchronizeWith(new RegulatedMotor[] { motor2 });
 	}
 	
-	public void move(int leftSpeed, int rightSpeed, int changeDirection) {
+	public void move(int leftSpeed, int rightSpeed, boolean changeDirection) {
 		motor1.setSpeed(leftSpeed);
 		motor2.setSpeed(rightSpeed);
 		motor1.startSynchronization();
-		if (changeDirection == 0) {
+		if (changeDirection) {
+			movingForward = !movingForward;
+		}
+		
+		if(movingForward) {
 			motor1.forward();
 			motor2.forward();
-		} else {
+		}else {
 			motor1.backward();
 			motor2.backward();
 		}
 
 		motor1.endSynchronization();
 	}
+	
 
 	public void stop() {
 		motor1.startSynchronization();
