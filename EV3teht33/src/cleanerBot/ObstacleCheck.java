86fb1f3d739782ce6fde6393148e92f59de1570c
jetrosa <jetrosa@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-06 10:42:06 +0000

    edited comments and access modifiers
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@49 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht33/src/cleanerBot/ObstacleCheck.java
+++ EV3teht33/src/cleanerBot/ObstacleCheck.java
@@ -2,44 +2,71 @@ package cleanerBot;
 
 import lejos.robotics.subsumption.Behavior;
 
+/**
+ * 
+ * @author jetro, kim, pietari, olli 6.9.2019
+ *
+ *         Behavior for the robot when it (IR sensor) detects an obstacle. The
+ *         robot moves backwards, turns and continues moving to the original
+ *         direction
+ */
 public class ObstacleCheck implements Behavior {
+	private final int detectionDistance = 10; // obstacle detection distance in centimeters
 	private volatile boolean suppressed = false;
-	DistanceSensor distanceSensor;
-	private Movement movement;
-	long backStartTime;
-	long turnStartTime;
+	private DistanceSensor distanceSensor; // IR sensor that measures the distance to obstacles/wall
+	private Movement movement; // robot movement controls
+	private long backStartTime;
+	private long turnStartTime;
 
+	/**
+	 * 
+	 * @param distanceSensor - reference to the IR sensor that measures distance
+	 * @param movement       - reference to the class that controls the motors
+	 */
 	public ObstacleCheck(DistanceSensor distanceSensor, Movement movement) {
 		this.distanceSensor = distanceSensor;
 		this.movement = movement;
 	}
-	
+
+	/**
+	 * Taking control when there is an obstacle closer than the defined distance
+	 */
 	@Override
 	public boolean takeControl() {
-		if(distanceSensor.distance()<10) {
+		if (distanceSensor.distance() < detectionDistance) {
 			return true;
 		}
-		return false;	
+		return false;
 	}
 
+	/**
+	 * Action that happens when the robot sees an obstacle. Robot moves backwards,
+	 * turns and continues moving to the original direction
+	 */
 	@Override
 	public void action() {
 		System.out.print("Obstacle detected");
 		suppressed = false;
-		
+
 		backStartTime = System.currentTimeMillis();
+
+		// moving forward for the defined time
 		movement.move(300, 300, true);
-		while(!suppressed && (System.currentTimeMillis()-backStartTime)<1000) {
+		while (!suppressed && (System.currentTimeMillis() - backStartTime) < 1000) {
 			Thread.yield();
 		}
 		turnStartTime = System.currentTimeMillis();
 		movement.tankTurn(300, 300, true);
-		while(!suppressed && (System.currentTimeMillis()-turnStartTime)<1000) {
+
+		// turning for the defined time
+		while (!suppressed && (System.currentTimeMillis() - turnStartTime) < 1000) {
 			Thread.yield();
 		}
+
+		// moving to the original direction
 		movement.move(300, 300, true);
-		
-		if(suppressed) {
+
+		if (suppressed) {
 			movement.stop();
 		}
 	}
@@ -48,5 +75,4 @@ public class ObstacleCheck implements Behavior {
 	public void suppress() {
 		suppressed = true;
 	}
-
 }
