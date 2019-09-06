da9dc240621972986805bc647fd83c08a5bf8f7f
jetrosa <jetrosa@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-06 11:16:41 +0000

    added comments
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@54 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht33/src/cleanerBot/DriveForward.java
+++ EV3teht33/src/cleanerBot/DriveForward.java
@@ -2,23 +2,42 @@ package cleanerBot;
 
 import lejos.robotics.subsumption.Behavior;
 
+/**
+ * Default behavior for the robot - it keeps driving forward when there are no
+ * other active behaviors
+ * 
+ * @author jetro, kim, pietari, olli
+ *
+ */
 public class DriveForward implements Behavior {
 	private volatile boolean suppressed = false;
-	private Movement movement;
+	private Movement movement; // movement reference (motor functions)
 
 	public DriveForward(Movement movement) {
 		this.movement = movement;
 	}
 
+	/**
+	 * Always ready to take control (default behavior)
+	 */
 	@Override
 	public boolean takeControl() {
 		return true;
 	}
 
+	/**
+	 * The robot drives forward (left and right motors) and stops the motor when
+	 * suppressed
+	 */
 	@Override
 	public void action() {
 		System.out.println("Moving forward");
 		suppressed = false;
+
+		/*
+		 * robot moves forward (predefined speed) until the behavior is suppressed and
+		 * then stops the motors
+		 */
 		movement.move(300, 300, false);
 		while (!suppressed)
 			Thread.yield();
