2de2800e7c4042f7e6f5f5e3f1587d52d9ae744d
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-09 09:47:14 +0000

    Added motor port test
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@66 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht33/src/cleanerBot/Main.java
+++ EV3teht33/src/cleanerBot/Main.java
@@ -11,37 +11,50 @@ import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
 public class Main {
 	public static void main(String[] args) {
 		Arbitrator arbitrator;
 		ArrayList<Behavior> behaviors = new ArrayList<>();
-		
+
 		// Gets the sensors and motors from the ports
 		Port colorPort = LocalEV3.get().getPort("S1");
 		Port IRPort = LocalEV3.get().getPort("S4");
-		RegulatedMotor motor1 = new EV3LargeRegulatedMotor(MotorPort.B);
-		RegulatedMotor motor2 = new EV3LargeRegulatedMotor(MotorPort.C);
+		Port motorPortLeft = MotorPort.B;
+		Port motorPortRight = MotorPort.C;
 
 		// Initializes the needed classes
 		ColorTester colorCalibrator = new ColorTester(colorPort);
 		DistanceSensor distanceSensor = new DistanceSensor(IRPort);
-		
+
 		// Testing that the IR sensor works properly
 		int rangeMin = 1;
 		int rangeMax = 10;
 		boolean testOK = false;
-		while(!testOK) {
+		while (!testOK) {
 			System.out.printf("Press ENTER near a wall (%d-%d cm)", rangeMin, rangeMax);
 			Button.ENTER.waitForPressAndRelease();
 			testOK = distanceSensor.testSensorRange(rangeMin, rangeMax);
 		}
-		if(testOK) {
+		if (testOK) {
 			System.out.println("IR range test OK");
 		}
-		
-		Movement movement = new Movement(motor1, motor2);
 
+		Movement movement = new Movement(motorPortLeft, motorPortRight);
+		// Testing if motors are connected to the ports. Loops if one or both of the
+		// motors are not detected.
+		boolean motorPortTest = false;
+		while (!motorPortTest) {
+			System.out.println("Testing motor ports");
+			if (movement.testMotorPorts()) {
+				System.out.println("\nMotor port test ok\n");
+				motorPortTest = true;
+			} else {
+				System.out.println("Check connection of motors");
+				Delay.msDelay(2000);
+			}
+		}
 		DriveForward driveForward = new DriveForward(movement);
 		LineCheck lineCheck = new LineCheck(colorCalibrator, movement);
 		ObstacleCheck obstacleCheck = new ObstacleCheck(distanceSensor, movement);
@@ -69,7 +82,7 @@ public class Main {
 
 		((Device) colorPort).close();
 		((Device) IRPort).close();
-		motor1.close();
-		motor2.close();
+		((Device) motorPortLeft).close();
+		((Device) motorPortRight).close();
 	}
 }
