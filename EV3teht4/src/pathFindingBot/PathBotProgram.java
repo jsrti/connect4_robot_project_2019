9db9dcd94c04e01ce56a4bfb2afa23891dd1028f
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-10 12:51:24 +0000

    Kalibrointi valmis
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@72 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht4/src/pathFindingBot/PathBotProgram.java
+++ EV3teht4/src/pathFindingBot/PathBotProgram.java
@@ -1,7 +1,22 @@
 package pathFindingBot;
 
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+
 public class PathBotProgram {
 	public static void main(String[] args) {
-	
+		final double HALKAISIJA = 3.1f;
+		final double RAIDELEVEYS = 20.3f;
+		RegulatedMotor mA = new EV3LargeRegulatedMotor(MotorPort.B);
+		RegulatedMotor mC = new EV3LargeRegulatedMotor(MotorPort.C);
+		DifferentialPilot pilotti = new DifferentialPilot(HALKAISIJA, RAIDELEVEYS, mA, mC);
+		// metri eteenp‰in
+		pilotti.travel(80);
+		// k‰‰nny kolme kierrosta oikealle
+		pilotti.rotate(4*360);
+		// kaarta pitkin: 50 cm s‰de, 90 asteen keskuskulma
+		pilotti.arc(50, 90);
 	}
 }
