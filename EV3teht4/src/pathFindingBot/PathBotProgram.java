c2cb2d67060fa09a756f176c365553119c1e579b
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-12 12:35:14 +0000

    Valmis koodi :)
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@81 892f24b9-3750-4648-92bf-123847ab498f

--- EV3teht4/src/pathFindingBot/PathBotProgram.java
+++ EV3teht4/src/pathFindingBot/PathBotProgram.java
@@ -2,27 +2,43 @@ package pathFindingBot;
 
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.Pose;
+import lejos.hardware.Button;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
+/**
+ * Robot that finds the shortest path to waypoints/goals and tries to avoid obstacles(walls).
+ * @author Pietari Järvi, Jetro Saarti, Kim Widberg, Olli Kaivola
+ *
+ */
 public class PathBotProgram {
 	public static void main(String[] args) {
-		final double HALKAISIJA = 3.2f;
-		final double RAIDELEVEYS = 17.4f;
-		final float robotRadius = 10f;
+		// Diameter of the robot's wheel used in movement calibration
+		final double DIAMETER = 3.2f;
+		// Robot's track width used in movement calibration
+		final double TRACKWIDTH = 18.0f;
+		// defines the area around the robot's middle point (approximately)
+		final float ROBOTRADIUS = 10f;
+		// Sets the robots starting position and angle
+		Pose startingPose = new Pose(43, 0, 90);
+
 		RegulatedMotor mA = new EV3LargeRegulatedMotor(MotorPort.B);
 		RegulatedMotor mC = new EV3LargeRegulatedMotor(MotorPort.C);
-		DifferentialPilot pilotti = new DifferentialPilot(HALKAISIJA, RAIDELEVEYS, mA, mC);
-		Kartta kartta = new Kartta();
-		// NaytteenOtto naytteenOtto = new NaytteenOtto();
-		Navigointi navigointi = new Navigointi(kartta.createDefaultMap(), pilotti, robotRadius);
+		DifferentialPilot pilot = new DifferentialPilot(DIAMETER, TRACKWIDTH, mA, mC);
+		Map map = new Map();
+		PathNavigating navigate = new PathNavigating(map.createDefaultMap(), pilot, ROBOTRADIUS, startingPose);
 
-		navigointi.lisaaPiste(12, 11);
-		navigointi.lisaaPiste(78, 11);
-		navigointi.lisaaPiste(39, 84);
-		navigointi.lisaaPiste(43, 0);
+		// Adding waypoints to the map
+		navigate.addWaypoint(12, 11);
+		navigate.addWaypoint(78, 11);
+		navigate.addWaypoint(39, 84);
+		navigate.addWaypoint(43, 0);
 
-		navigointi.aloitaNavigointi();
+		// Button check before starting pathfinding
+		System.out.println("Press ENTER to start");
+		Button.ENTER.waitForPressAndRelease();
 
+		navigate.startNavigating();
 	}
 }
