20c3bdc364c8f2d759abfb575e0e0c7caac80b5f
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-17 12:37:18 +0000

    Kommentteja
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@96 892f24b9-3750-4648-92bf-123847ab498f

--- EV3Teht5Robot/src/pathFindingBot/Communication.java
+++ EV3Teht5Robot/src/pathFindingBot/Communication.java
@@ -14,6 +14,13 @@ import lejos.robotics.mapping.LineMap;
 import lejos.robotics.navigation.Pose;
 import lejos.robotics.navigation.Waypoint;
 
+/**
+ * Class contains methods for controlling the data streams (sending and
+ * receiving data) between the robot and PC (robot side)
+ * 
+ * @author Pietari Järvi, Olli Kaivola, Jetro Saarti, Kim Widberg
+ *
+ */
 public class Communication {
 
 	private ServerSocket serveri;
@@ -22,6 +29,10 @@ public class Communication {
 	private DataOutputStream out = null;
 	private ObjectOutputStream oos = null;
 
+	/**
+	 * Opens server socket connection and data streams and waits for the PC to
+	 * connect
+	 */
 	public void openConnection() {
 		try {
 			serveri = new ServerSocket(1111);
@@ -36,7 +47,10 @@ public class Communication {
 		}
 		System.out.println("CONNECTED");
 	}
-	
+
+	/**
+	 * Waits for the start command from PC
+	 */
 	public void waitForGoCommand() {
 		try {
 			in.readInt();
@@ -45,20 +59,30 @@ public class Communication {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Sends sample from waypoint to PC
+	 * 
+	 * @param s
+	 */
 	public void sendSample(Sample s) {
 		try {
 			oos.writeObject(s);
 			oos.flush();
 			System.out.println("Sample sent");
-		}catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println("Couldn't send sample");
 		}
 	}
-	
+
+	/**
+	 * Receives the amount of waypoints that PC is going to send
+	 * 
+	 * @return
+	 */
 	public int receiveWaypointCount() {
-		int count = 0; 
-		
+		int count = 0;
+
 		try {
 			count = in.readInt();
 		} catch (IOException e) {
@@ -67,38 +91,53 @@ public class Communication {
 		}
 		return count;
 	}
-	
-	public Waypoint receiveWaypoint(){
-		Waypoint point = new Waypoint(0,5);
+
+	/**
+	 * Receives waypoint coordinates sent from the PC
+	 * 
+	 * @return
+	 */
+	public Waypoint receiveWaypoint() {
+		Waypoint point = new Waypoint(0, 5);
 		try {
 			point.loadObject(in);
 			System.out.println("X " + point.x);
 			System.out.println("Y " + point.y);
-		}catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println("Couldn't load waypoints");
 			System.err.println(e);
 		}
 		return point;
 	}
-	
+
+	/**
+	 * Receives the LineMap object sent from the PC
+	 * 
+	 * @return
+	 */
 	public LineMap receiveLineMap() {
 		LineMap map = new LineMap();
 		try {
 			map.loadObject(in);
 			System.out.println("Map loaded");
-		}catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println("Couldn't load map");
 			System.err.println(e);
 		}
 		return map;
 	}
-	
+
+	/**
+	 * Receives the Pose object sent from the PC
+	 * 
+	 * @return
+	 */
 	public Pose receivePose() {
 		Pose startingPose = new Pose();
 		try {
 			startingPose.loadObject(in);
 			System.out.println("Starting pose loaded");
-		}catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println("Couldn't load starting pose");
 			System.err.println(e);
 		}
