f58c0e0dcad7df92fa0a8ad0824f30adbb43d266
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-27 09:00:13 +0000

    robot communication
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@113 892f24b9-3750-4648-92bf-123847ab498f

--- Connect4PC/src/pcRobotHost/CommunicationPC.java
+++ Connect4PC/src/pcRobotHost/CommunicationPC.java
@@ -1,12 +1,13 @@
 package pcRobotHost;
 
+import java.awt.Point;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 
 /**
  * 
@@ -17,8 +18,9 @@ public class CommunicationPC {
 	private DataOutputStream out = null;
 	private DataInputStream in = null;
 	private ObjectInputStream oIn = null;
+	private ObjectOutputStream oos = null;
 	private Socket s = null;
-	
+
 	/**
 	 * Opens socket connection and data streams
 	 */
@@ -27,14 +29,15 @@ public class CommunicationPC {
 			s = new Socket("10.0.1.1", 1111);
 			out = new DataOutputStream(s.getOutputStream());
 			in = new DataInputStream(s.getInputStream());
-			oIn = new ObjectInputStream(in); 
+			oIn = new ObjectInputStream(in);
+			oos = new ObjectOutputStream(out);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Closes socket connection
 	 */
@@ -45,7 +48,7 @@ public class CommunicationPC {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Start command for robot
 	 */
@@ -56,6 +59,65 @@ public class CommunicationPC {
 		} catch (IOException e) {
 			System.err.println(e);
 			System.out.println("Failed to send waypoint count");
+		}
+	}
+
+	/**
+	 * Receives the gameboard array from the robot
+	 * 
+	 * @return
+	 */
+	/*
+	public int[][] receiveGameboardArray() {
+		int[][] gameBoardArray = null;
+		try {
+			gameBoardArray = (int[][]) oIn.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return gameBoardArray;
+	}
+*/
+	/**
+	 * Sends a point object to the robot
+	 * 
+	 * @param point
+	 */
+	/*
+	public void sendDropPoint(Point point) {
+		try {
+			oos.writeObject(point);
+			oos.flush();
+			System.out.println("Point sent");
+		} catch (IOException e) {
+			System.out.println("Couldn't send point");
+		}
+	}
+*/
+	/**
+	 * 
+	 * @return
+	 */
+	public int receiveGamePieceX() {
+		int gamePieceX = 0;
+
+		try {
+			gamePieceX = in.readInt();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return gamePieceX;
+	}
+
+	public void sendGamePieceX(int gamePieceX) {
+		try {
+			out.writeInt(gamePieceX);
+			out.flush();
+		} catch (IOException e) {
+			System.err.println(e);
+			System.out.println("Failed to send game piece");
 		}
 	}
 }
