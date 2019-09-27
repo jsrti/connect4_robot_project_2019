4bdbe0b0eb159a62c45fb7ab2fbf785da04a3220
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-09-27 09:00:49 +0000

    robot commit
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@114 892f24b9-3750-4648-92bf-123847ab498f

--- Connect4Robot/src/connect4/Communication.java
+++ Connect4Robot/src/connect4/Communication.java
@@ -1,14 +1,15 @@
 package connect4;
 
+import java.awt.Point;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import lejos.hardware.Sound;
-
 
 /**
  * Class contains methods for controlling the data streams (sending and
@@ -24,6 +25,7 @@ public class Communication {
 	private DataInputStream in = null;
 	private DataOutputStream out = null;
 	private ObjectOutputStream oos = null;
+	private ObjectInputStream oin = null;
 
 	/**
 	 * Opens server socket connection and data streams and waits for the PC to
@@ -38,6 +40,7 @@ public class Communication {
 			in = new DataInputStream(s.getInputStream());
 			out = new DataOutputStream(s.getOutputStream());
 			oos = new ObjectOutputStream(out);
+			oin = new ObjectInputStream(in);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -53,6 +56,69 @@ public class Communication {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	
+	/**
+	 * Sends gameboard array to PC
+	 * 
+	 * @param gameBoardArray
+	 */
+	/*
+	public void sendGameboardArray(int[][] gameBoardArray) {
+		try {
+			oos.writeObject(gameBoardArray);
+			oos.flush();
+			System.out.println("Gameboard sent");
+		} catch (IOException e) {
+			System.out.println("Couldn't send gameboard");
+		}
+	}
+	*/
+	/**
+	 * Receives point coordinates sent from the PC
+	 * 
+	 * @return
+	 */
+	/*
+	public Point receiveDropPoint() {
+		Point point = new Point(0, 0);
+		try {
+			point = (Point) oin.readObject();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return point;
+	}
+	*/
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int receiveGamePieceX() {
+		int gamePieceX = 0;
+
+		try {
+			gamePieceX = in.readInt();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return gamePieceX;
+	}
+
+	public void sendGamePieceX(int gamePieceX) {
+		try {
+			out.writeInt(gamePieceX);
+			out.flush();
+		} catch (IOException e) {
+			System.err.println(e);
+			System.out.println("Failed to send game piece");
 		}
 	}
 }
