50d0d3f02aa3fa1285d0e3ccb68e0619ef2a84db
penttipj <penttipj@892f24b9-3750-4648-92bf-123847ab498f>
2019-10-09 12:25:42 +0000

    jotenki toimii
    git-svn-id: https://innoscm.edusrv.metropolia.fi/svn/lego2020s7/trunk@170 892f24b9-3750-4648-92bf-123847ab498f

--- Connect4PC2/src/model/CommunicatorTask.java
+++ Connect4PC2/src/model/CommunicatorTask.java
@@ -11,29 +11,43 @@ import view.GameOverviewController;
 public class CommunicatorTask extends Task<Void> {
 
 	private boolean gameRunning = true;
-	private CommunicationPC comm;
-	private GameOverviewController con;
-	
+	private CommunicationPC communicationPC;
+	private GameOverviewController gameOverviewController;
+	private Determinator determinator;
+	private Board board;
 
-	public CommunicatorTask(CommunicationPC comm, GameOverviewController con) {
-		this.comm = comm;
-		comm.openConnection();
-		this.con = con;
+	private int playerNumber = 2;
+	private int robotNumber = 1;
+
+	public CommunicatorTask(CommunicationPC communicationPC, GameOverviewController gameOverviewController) {
+		this.communicationPC = communicationPC;
+		communicationPC.openConnection();
+		this.gameOverviewController = gameOverviewController;
+		board = new Board();
+		determinator = new Determinator(board, robotNumber, playerNumber);
 	}
 
 	@Override
 	protected Void call() throws Exception {
-		
-		while (gameRunning) {	
-				System.out.println("kuunnellaan vuoron va");
-				comm.receiveTurnChange();
-				System.out.println("Turn change received");
-				Point p = comm.receiveDropPoint();
-				System.out.println("Point received - x:" + p.x + " y:" + p.y);
-				con.addPlayerPiece(p);
-			}
 
-		comm.closeConnection();
+		while (gameRunning) {
+			System.out.println("kuunnellaan vuoron va");
+			communicationPC.receiveTurnChange();
+			System.out.println("Turn change received");
+			Point p = communicationPC.receiveDropPoint();
+			System.out.println("Point received - x:" + p.x + " y:" + p.y);
+			// con.addPlayerPiece(p);
+			board.setPiece(p.x, playerNumber);
+			Tester.printBoard(board);
+			Point calculatedDropPoint = determinator.getNextMove();
+			board.setPiece(calculatedDropPoint.x, robotNumber);
+			communicationPC.sendDropPoint(calculatedDropPoint);
+			Tester.printBoard(board);
+			System.out.println("Sent point x: "+calculatedDropPoint.x + " y: "+calculatedDropPoint.y);
+
+		}
+
+		communicationPC.closeConnection();
 		return null;
 	}
 }
